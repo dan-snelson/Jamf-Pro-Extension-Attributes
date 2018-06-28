c9057bbfb9d0fdfb28adeb0b10be78fa8d5b9928
Dan K. Snelson <dan@snelson.us>
2018-06-28 05:53:16 -0500

    IP address of Cisco AnyConnect VPN client

---
+++ Cisco VPN IP.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+###########################################################################
+# Extension Attribute to read the IP address of the active VPN connection #
+###########################################################################
+
+RESULT="Not Installed"
+
+testFile="/opt/cisco/anyconnect/bin/vpn" # Cisco VPN binary
+
+if [[ -f "${testFile}" ]] ; then
+	# Cisco AnyConnect installed; read current IP Address
+	RESULT=$(/opt/cisco/anyconnect/bin/vpn stats | grep "Client Address (IPv4):" | awk '{print $4}')
+
+	if [[ "${RESULT}" == "" ]]; then
+		RESULT="No VPN Connection"
+	fi
+
+fi
+
+echo "<result>$RESULT</result>"
+
+exit 0
